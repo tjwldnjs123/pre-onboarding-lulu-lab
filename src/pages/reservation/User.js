3c6704aa9c1590530d2e987d4ac685b9ffb2dcc7
tjwldnjs123 <tjwldnjs12345@naver.com>
2022-10-14 19:50:57 +0900

    Add: 회원정보 페이지 레이아웃

---
+++ src/pages/reservation/User.js
@@ -0,0 +1,75 @@
+import React from "react";
+import styled from "styled-components";
+
+const User = () => {
+  return (
+    <UserContainer>
+      <p>회원 정보</p>
+      <div className="user-input">
+        <div className="user-name">
+          <label htmlFor="userName">환자 이름 </label>
+          <input type="text" id="userName" />
+        </div>
+        <div className="user-phone">
+          <label htmlFor="phoneNum">핸드폰 번호 </label>
+          <input type="number" id="phoneNum" />
+        </div>
+      </div>
+      <div className="btn">
+        <button>확인</button>
+        <button>취소</button>
+      </div>
+    </UserContainer>
+  );
+};
+
+const UserContainer = styled.div`
+  width: 100%;
+  margin: auto;
+
+  p {
+    padding: 8%;
+    text-align: center;
+    font-size: 25px;
+  }
+
+  .user-input {
+    width: 70%;
+    border: 1px solid black;
+    display: flex;
+    flex-direction: column;
+    margin: auto;
+    font-size: 19px;
+
+    input {
+      width: 70%;
+      height: 30px;
+    }
+
+    .user-name {
+      padding: 10%;
+
+      label {
+        margin-right: 17px;
+      }
+    }
+    .user-phone {
+      padding: 10%;
+    }
+  }
+
+  .btn {
+    width: 70%;
+    border: 1px solid red;
+    padding: 2%;
+    margin: auto;
+    text-align: center;
+
+    button {
+      padding: 10px 20px;
+      margin: 3%;
+    }
+  }
+`;
+
+export default User;
