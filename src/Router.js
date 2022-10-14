ddaca5c5c4befa90b85fc434e67e63ca9e32efb4
tjwldnjs123 <tjwldnjs12345@naver.com>
2022-10-14 18:06:30 +0900

    Add: 메인페이지 폴더 생성

--- src/Router.js
+++ src/Router.js
@@ -1,13 +1,15 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/main/Home";
 
 const Router = () => {
-  return;
-  <BrowserRouter>
-    <Routes>
-      <Route />
-    </Routes>
-  </BrowserRouter>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </BrowserRouter>
+  );
 };
 
 export default Router;
